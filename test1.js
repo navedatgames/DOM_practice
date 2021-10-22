7b6dcb8e5c33c582d6114366a0289189aaff9777
Naved <navedatgames@gmail.com>
2021-10-22 15:53:38 +0530

    some test

---
+++ test1.js
@@ -0,0 +1,8 @@
+let obj= {"name":"Naved",
+        "age":17,"address":"greater noida"};
+
+
+let o = {name:"naved",age:20,address:"greater Noida",
+        phone:"7266921985"}
+
+console.log(Object.values(o));
