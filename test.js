5d234cc6906b0298598824a206dc47b7c3f743ed
Naved <navedatgames@gmail.com>
2021-10-30 16:13:15 +0530

    new file

---
+++ test.js
@@ -0,0 +1,21 @@
+let obj = [
+    {
+        firstName:"jack",
+        lastName:"synder",
+        number:456789
+    },
+    {
+        firstName:"naved",
+        lastName:"khan",
+        number:748596
+    },
+    {
+        firstName:"syam",
+        lastName:"sudat",
+        number:859674,
+        likes:["java","unity","design"]
+    }
+
+]
+
+console.log(obj[2]["likes"][0]);
